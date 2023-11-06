0cc142f7ad9dbab62f5c2de4b3b93fc25ac7950f
Mayank <mayank0real0world@gmail.com>
2023-11-06 21:19:12 +0530

    add : added API error Files

--- src/index.js
+++ src/index.js
@@ -6,9 +6,15 @@ import connectdb from "./db/index.js";
 dotenv.config({path: './env'})
 
 connectdb()
-
-
-
+.then(()=>{
+    app.listen(process.env.PORT || 8000 ,()=>{
+        console.log(`Server is running at port : ${process.env.PORT}`);
+        app.on("error",(error)=>{console.log("Error : ",error); throw error ;} )
+    })
+})
+.catch((error)=>{
+console.log("MONGO db connection failed: " + error);
+})
 
 
 
